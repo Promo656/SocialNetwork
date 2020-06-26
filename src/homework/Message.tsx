2e168f7f390185bdc3fc09ac91f1ae3cfadef347
Promo656 <rank2007@mail.ru>
2020-06-26 23:35:56 +0300

    commit

--- src/homework/Message.tsx
+++ src/homework/Message.tsx
@@ -9,14 +9,15 @@ type MessageType = {
 
 function Message(props: MessageType) {
     return (
-        <div className={s.message}>
-            <div className={s.messageLogo}><img src="https://fanparty.ru/fanclubs/tom-kruz/gallery/1851014_tom_kruz.jpg"
-                                                alt=""/></div>
-
-            <div className={s.name}>{props.name}</div>
-            <div className={s.text}>{props.text}</div>
-            <div className={s.time}>{props.time}</div>
-            <div className={s.tail}></div>
+        <div className={s.messageBlock}>
+            <img className={s.messageLogo} src="https://fanparty.ru/fanclubs/tom-kruz/gallery/1851014_tom_kruz.jpg"
+                 alt=""/>
+            <div className={s.message}>
+                <div className={s.name}>{props.name}</div>
+                <div className={s.text}>{props.text}</div>
+                <div className={s.time}>{props.time}</div>
+                <div className={s.tail}></div>
+            </div>
         </div>
 
 )
