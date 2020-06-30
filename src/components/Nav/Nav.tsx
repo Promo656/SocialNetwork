32ab747431a2920995808a86a95a120a0330775d
Promo656 <rank2007@mail.ru>
2020-06-30 23:11:43 +0300

    commit

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import s from "./Nav.module.css"
+import {NavLink} from "react-router-dom";
 
 export function Nav() {
     return (
             <nav className={s.nav}>
                 <ul className={s.textUl}>
-                    <li><a href="/profile">Profile</a></li>
-                    <li><a href="/dialogs">Messages</a></li>
-                    <li><a href="/news">News</a></li>
-                    <li><a href="/music">Music</a></li>
-                    <li><a href="/settings">Settings</a></li>
+                    <li><NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink></li>
+                    <li><NavLink to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink></li>
+                    <li><NavLink to="/news" activeClassName={s.activeLink}>News</NavLink></li>
+                    <li><NavLink to="/music" activeClassName={s.activeLink}>Music</NavLink></li>
+                    <li><NavLink to="/settings" activeClassName={s.activeLink}>Settings</NavLink></li>
                 </ul>
             </nav>
     )
