06d2355352ebe30a786272c6ecbf669b23c60682
Promo656 <rank2007@mail.ru>
2020-07-16 22:50:09 +0300

    commit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,6 +6,7 @@ export function Header() {
         <div>
             <header className={s.header}>
                 <img className={s.photo} src="https://cdn.pixabay.com/photo/2014/04/02/17/01/logo-307673_1280.png" alt=""/>
+                <input className={s.search} type="text" name={s.search} id={s.search}/>
             </header>
         </div>
     )
