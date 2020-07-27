a1cd9a4be8a25b6107698a3e68849e1339052979
Egor <rank2007@mail.ru>
2020-07-27 23:01:41 +0300

    Updates

---
+++ precache-manifest.147b280f6a9058aa1d67bdbe18835adb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b458c1a17849ecd65da9736b9db43281",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "0c118b7e46196641f76a",
+    "url": "/SocialNetwork/static/css/main.10401527.chunk.css"
+  },
+  {
+    "revision": "b107abbf95049948ec15",
+    "url": "/SocialNetwork/static/js/2.1c3bc51f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SocialNetwork/static/js/2.1c3bc51f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c118b7e46196641f76a",
+    "url": "/SocialNetwork/static/js/main.fbc0aa2e.chunk.js"
+  },
+  {
+    "revision": "2c214d47168349e506c3",
+    "url": "/SocialNetwork/static/js/runtime-main.ad3e0edf.js"
+  }
+]);
