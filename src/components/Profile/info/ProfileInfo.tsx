5b38a9c09b21607669be4beb549abdbe095fc3ad
Egor <rank2007@mail.ru>
2020-08-20 22:43:10 +0300

    Commit

--- src/components/Profile/info/ProfileInfo.tsx
+++ src/components/Profile/info/ProfileInfo.tsx
@@ -16,20 +16,3 @@ export function ProfileInfo() {
         </div>
     )
 }
-
-{/*<div className={s.BGPhoto}>
-            </div>
-            <div className={s.aboutPerson}>
-                <div className={s.PPhoto}>
-                </div>
-                <div className={s.description}>
-                    <ul>
-                        <li className={s.name}>Egor P.</li>
-                        <li>Date of Birth: 1994</li>
-                        <li>City: Moscow</li>
-                        <li>Education: MSU</li>
-                        <li>Website: github</li>
-                    </ul>
-                </div>
-            </div>*/
-}
